832ed95980025948e3470b1fa50bde9da2bc336a
slavko <slavkoj6@gmail.com>
2021-03-30 14:54:55 +0200

    init commit

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -1,6 +1,21 @@
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup } from "react-bootstrap";
+import React from "react";
 
-const DishComments = (props) =>
+class DishComments extends React.Component {
+  render() {
+    return this.state.selectedDish ? (
+      <ListGroup>
+        <h2>Comments for {this.state.selectedDish.name}</h2>
+        {this.state.selectedDish.comments.map((c) => (
+          <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    ) : (
+      <h1>NOTHING YET</h1>
+    );
+  }
+}
+/* const DishComments = (props) =>
     props.selectedDish
         ? <ListGroup>
             <h2>Comments for {props.selectedDish.name}</h2>
@@ -10,9 +25,8 @@ const DishComments = (props) =>
                 ))
             }
         </ListGroup>
-        : <h1>NOTHING YET</h1>
+        : <h1>NOTHING YET</h1> */
 
-
-export default DishComments
+export default DishComments;
 
 // mapping props.selectedDish.comments
